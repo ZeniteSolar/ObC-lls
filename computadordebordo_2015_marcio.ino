d5b4e4bee2e35e9d734009dd21903e3033e71b28
joaoantoniocardoso <joao.maker@gmail.com>
2015-10-29 21:36:32 -0200

    Media testada, versao compativel com placa 2014.

--- computadordebordo_2015_marcio.ino
+++ computadordebordo_2015_marcio.ino
@@ -8,7 +8,7 @@
 #define batVoltPin A2			// A2: Battery Voltage Sensor
 #define mpptCurPin A3			// A3: MPPT Current Sensor
 #define motorCurPin A4			// A4: Motor Current Sensor
-#define potPin A5				// A5: Potentiometer Voltage
+#define potPin A5			// A5: Potentiometer Voltage
 
 unsigned int batTemp;
 unsigned int motorTemp;
@@ -33,6 +33,24 @@ void sendDataCallBack();
 void setup(){
 	Serial.begin(9600);
 
+        pinMode(2, OUTPUT);
+        pinMode(3, OUTPUT);
+        pinMode(4, OUTPUT);
+	pinMode(5, OUTPUT);
+        pinMode(6, OUTPUT);
+        pinMode(7, OUTPUT);
+        pinMode(8, OUTPUT);
+        pinMode(9, OUTPUT);
+        pinMode(12, OUTPUT);
+        pinMode(13, OUTPUT);
+        
+        pinMode(A0, INPUT);
+        pinMode(A1, INPUT);
+        pinMode(A2, INPUT);
+        pinMode(A3, INPUT);
+        pinMode(A4, INPUT);
+        pinMode(A5, INPUT);
+
 	// Configure Threads.
 	measuresThread->onRun(measuresCallBack);
 	measuresThread->setInterval(100);
@@ -47,7 +65,7 @@ void setup(){
 	initialTime1 = micros();
 	initialTime2 = micros();
 	interrupts();
-	Serial.println("setup");
+        
 }
 
 void loop(){
@@ -64,6 +82,7 @@ void loop(){
 	oldTime2 = actualTime2;
 	// Serial.print(actualTime1 - oldTime1); // debug
 	interrupts();
+	
 }
 
 void sendDataCallBack(){
@@ -71,25 +90,28 @@ void sendDataCallBack(){
 	if (Serial.available()) {
 		//noInterrupts();
 		byte c = Serial.read();
-		float a = 0;
 		// if an ACK is received, send it back
 		if (c == 0x01 ){
-			Serial.write(c); // send ACK back      
+			Serial.write(c); // send ACK back   
 			switch (sensor) {
 				case 0xA0: // Battery Temperature Sensor
-					BluetoothSend(mapFloat( (float)batTemp, 0, 1023, 0, 5));
+//					BluetoothSend(mapFloat((long)batTemp,0,1023,0,150));
+                                        BluetoothSend(32.4f); //FAKE DATA FOR DEBUG
 					sensor ++;
 					break;
-				case 0xA1: // Battery Temperature Sensor
-					BluetoothSend(mapFloat( (float)motorTemp, 0, 1023, 0, 5));
+				case 0xA1: // Motor Temperature Sensor
+//					BluetoothSend(mapFloat((long)motorTemp,0,1023,0,150));
+                                        BluetoothSend(51.9f); //FAKE DATA FOR DEBUG
 					sensor ++;
 					break;
 				case 0xA2: // Battery Voltage Sensor
-					BluetoothSend(mapFloat( (float)batVolt, 0, 1023, 0, 5)); //48
+//					BluetoothSend(mapFloat((long)batVolt,0,1023,0,48));
+                                        BluetoothSend(25.8f); //FAKE DATA FOR DEBUG
 					sensor ++;
 					break;
 				case 0xA3: // MPPT Current Sensor
-					BluetoothSend(mapFloat( (float)mpptCurxTime / (micros() - initialTime1), 0, 1023, 0, 5));
+					BluetoothSend(mapFloat((long)mpptCurxTime,0,1023,0,150));
+//					BluetoothSend(0.1f); //FAKE DATA FOR DEBUG
 					// Serial.print("\n Current1:  "); // debug
 					// Serial.print(mapFloat( (float)mpptCurxTime / (micros() - initialTime1), 0, 1023, 0, 5)); // debug
 					// Serial.print("\t DeltaTime:  "); // debug
@@ -99,7 +121,8 @@ void sendDataCallBack(){
 					sensor ++;
 					break;
 				case 0xA4: // Motor Current Sensor
-					BluetoothSend(mapFloat( (float)motorCurxTime / (micros() - initialTime2), 0, 1023, 0, 5));
+					BluetoothSend(mapFloat((long)motorCurxTime,0,1023,0,150));
+//					BluetoothSend(200.0f); //FAKE DATA FOR DEBUG
 					// Serial.print("\t\t Current2:"); // debug
 					// Serial.print(mapFloat( (float)motorCurxTime / (micros() - initialTime2), 0, 1023, 0, 5)); // debug
 					// Serial.print("\t DeltaTime:  "); // debug
@@ -109,7 +132,8 @@ void sendDataCallBack(){
 					sensor ++;
 					break;
 				case 0xA5: // Potentiometer Voltage
-					BluetoothSend(mapFloat( (float)potVolt, 0, 1023, 0, 5));
+//					BluetoothSend(2.0f); //FAKE DATA FOR DEBUG
+                                        BluetoothSend(mapFloat((long)batVolt,0,1023,0,5));
 					sensor = 0xA0;
 					break;
 				default: 
@@ -136,6 +160,8 @@ void BluetoothSend(float value){
 }
 
 // A copy of "map" function working with float values.
-float mapFloat(float value, int in_min, int in_max, int out_min, int out_max){
-	return (float) (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+float mapFloat(long value, int in_min, int in_max, int out_min, int out_max){
+	return (float) ( (value - in_min) * (out_max - out_min) )/( (float) (in_max - in_min) ) + out_min;
 }
+
+
